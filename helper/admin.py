6c75d0045f59f61aaeb2550488a3ea68bfd5bb5a
Ivan <mrkvvn@yandex.ru>
2022-10-04 23:09:06 +0300

    Database update, adding new attributes

--- helper/admin.py
+++ helper/admin.py
@@ -4,10 +4,10 @@ from .models import Helper
 # Register your models here.
 
 class HelperAdmin(admin.ModelAdmin):
-    list_display  = ('id', 'title', 'description', 'date', 'done')
-    list_display_links = ('id', 'title')
-    search_fields = ('id', 'title', 'description')
-    list_editable = ('done',)
-    list_filter = ('done',)
+    list_display  = ('id', 'task', 'date', 'done', 'priority', 'delete')
+    list_display_links = ('id', 'task')
+    search_fields = ('id', 'task')
+    list_editable = ('done', 'delete')
+    list_filter = ('done', 'date', 'priority', 'delete')
 
 admin.site.register(Helper, HelperAdmin)
--- helper/models.py
+++ helper/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Helper(models.Model):
-    title = models.CharField(max_length=150)
-    description = models.CharField(max_length=300, blank=True)
+    task = models.CharField(max_length=500, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     done = models.BooleanField(default=False)
+    priority = models.CharField(max_length=1, blank=True)
+    delete = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return self.task
